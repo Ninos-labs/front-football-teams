5d98588cb63c3ba85ba94bf11f663961300b48b0
manuelitox <manuel2788@gmail.com>
2020-11-11 20:19:56 +0100

    Improves design of the BoxTitleStyled component

--- src/components/boxTitle/boxTitle.styled.tsx
+++ src/components/boxTitle/boxTitle.styled.tsx
@@ -1,7 +1,12 @@
 import styled from 'styled-components';
 
 const BoxTitleStyled = styled.h2`
+  background-color: ${({ theme }) => theme.colors?.white};
   font-size: ${({ theme }) => theme.fonts?.sizes?.subTitle};
+  top: 0;
+  padding: 2rem 0;
+  position: sticky;
+  z-index: 1;
 `;
 
 export default BoxTitleStyled;
