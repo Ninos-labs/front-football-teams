2bc978b047005142322ba042399bee43785ccf14
manuelitox <manuel2788@gmail.com>
2020-11-17 19:50:16 +0100

    create a data formatter for matches filtered by At

---
+++ src/utils/format-matches-avg-by-at/format-matches-avg-by-at.ts
@@ -0,0 +1,35 @@
+import {
+  AtTypes,
+  ItemTypes,
+  DataTypes
+} from './format-matches-avg-by-at.types';
+
+export const byAt = (item: ItemTypes, at: AtTypes) =>
+  item.description.includes(at);
+
+export const itemFormatted = (item: ItemTypes, at: AtTypes) => ({
+  name: item.description.replace(`,${at}`, ''),
+  value: Number(item.value.toFixed(2))
+});
+
+export const formatMatchesAvgByAt = (data: ItemTypes[]) => {
+  const home: DataTypes = { name: 'Home' };
+  data
+    .filter((item) => byAt(item, 'Home'))
+    .map((item) => itemFormatted(item, 'Home'))
+    .map((item) => (home[item.name] = item.value));
+
+  const abroad: DataTypes = { name: 'Abroad' };
+  data
+    .filter((item) => byAt(item, 'Abroad'))
+    .map((item) => itemFormatted(item, 'Abroad'))
+    .map((item) => (abroad[item.name] = item.value));
+
+  const neutral: DataTypes = { name: 'Neutral' };
+  data
+    .filter((item) => byAt(item, 'Neutral'))
+    .map((item) => itemFormatted(item, 'Neutral'))
+    .map((item) => (neutral[item.name] = item.value));
+
+  return [home, abroad, neutral];
+};
---
+++ src/utils/format-matches-avg-by-at/index.ts
@@ -0,0 +1,1 @@
+export * from './format-matches-avg-by-at';
