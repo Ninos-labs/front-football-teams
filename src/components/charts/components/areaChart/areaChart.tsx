b667b1992e484c2ab1087fd2987887a66c10cf9d
manuelitox <manuel2788@gmail.com>
2020-11-23 22:30:00 +0100

    remove unnecessary charts attributes and add div tag wrapper

--- src/components/charts/components/areaChart/areaChart.tsx
+++ src/components/charts/components/areaChart/areaChart.tsx
@@ -5,32 +5,25 @@ import { AreaChart, Area, XAxis, CartesianGrid, Tooltip, ResponsiveContainer } f
 import { AreaChartTypes } from './areaChart.types';
 
 const WrapperAreaChart = ({ data, items, datakey, customTooltip }: AreaChartTypes) => (
-  <ResponsiveContainer minWidth="600" minHeight="200px">
-    <AreaChart
-      data={data}
-      stackOffset="expand"
-      margin={{
-        top: 10,
-        right: 30,
-        left: 0,
-        bottom: 0
-      }}
-    >
-      <CartesianGrid strokeDasharray="2 2" />
-      <XAxis dataKey={datakey} />
-      <Tooltip content={customTooltip} />
-      {items.map((item, index) => (
-        <Area
-          key={index}
-          type="monotone"
-          dataKey={item.name}
-          stackId="1"
-          fill={item.fill}
-          stroke={item.fill}
-        />
-      ))}
-    </AreaChart>
-  </ResponsiveContainer>
+  <div>
+    <ResponsiveContainer minWidth="600" minHeight="200px">
+      <AreaChart data={data} stackOffset="silhouette">
+        <CartesianGrid strokeDasharray="2 2" />
+        <XAxis dataKey={datakey} />
+        <Tooltip content={customTooltip} />
+        {items.map((item, index) => (
+          <Area
+            key={index}
+            type="monotone"
+            dataKey={item.name}
+            stackId="1"
+            fill={item.fill}
+            stroke={item.fill}
+          />
+        ))}
+      </AreaChart>
+    </ResponsiveContainer>
+  </div>
 );
 
 WrapperAreaChart.propTypes = {
--- src/components/charts/components/barChart/barChart.tsx
+++ src/components/charts/components/barChart/barChart.tsx
@@ -5,26 +5,18 @@ import { XAxis, CartesianGrid, Tooltip, BarChart, Bar, ResponsiveContainer } fro
 import { BarChartTypes } from './barChart.types';
 
 const WrapperBarChart = ({ data, items, xAxis, customTooltip }: BarChartTypes) => (
-  <ResponsiveContainer minWidth="600" minHeight="200px">
-    <BarChart
-      width={600}
-      height={200}
-      data={data}
-      margin={{
-        top: 20,
-        right: 30,
-        left: 20,
-        bottom: 5
-      }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey={xAxis} />
-      <Tooltip content={customTooltip} />
-      {items.map((item, index) => (
-        <Bar key={index} dataKey={item.name} stackId="a" fill={item.fill} />
-      ))}
-    </BarChart>
-  </ResponsiveContainer>
+  <div>
+    <ResponsiveContainer minWidth="600" minHeight="200px">
+      <BarChart data={data}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey={xAxis} />
+        <Tooltip content={customTooltip} />
+        {items.map((item, index) => (
+          <Bar key={index} maxBarSize={40} dataKey={item.name} stackId="a" fill={item.fill} />
+        ))}
+      </BarChart>
+    </ResponsiveContainer>
+  </div>
 );
 
 WrapperBarChart.propTypes = {
