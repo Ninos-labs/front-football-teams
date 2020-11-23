da7617bd763752697a6273c98f137158917ab0d7
manuelitox <manuel2788@gmail.com>
2020-11-23 21:40:36 +0100

    add venezuela model

--- src/modules/ven/ven.tsx
+++ src/modules/ven/ven.tsx
@@ -3,15 +3,13 @@ import React, { Fragment } from 'react';
 import matches from 'data/ven/full-matches.json';
 import resultsAvg from 'data/ven/results-avg.json';
 import goalsByTeam from 'data/ven/goals-by-team.json';
-import goalsByTeamAvg from 'data/ven/goals-by-team-avg.json';
 import goalsAvgByYear from 'data/ven/goals-avg-by-year.json';
 import resultsAvgByYear from 'data/ven/results-avg-by-year.json';
 import matchesAvgByAt from 'data/ven/matches-avg-by-at.json';
 import matchWithMostGoalsFor from 'data/ven/most-goals-for.json';
 import matchWithMostGoalsAgainst from 'data/ven/most-goals-against.json';
-
 import winsByTeam from 'data/ven/wins-by-team.json';
-import drawByTeam from 'data/ven/draws-by-team.json';
+import drawsByTeam from 'data/ven/draws-by-team.json';
 import lossesByTeam from 'data/ven/losses-by-team.json';
 import goalsMaxAndMin from 'data/ven/goals-max-min.json';
 
@@ -25,59 +23,24 @@ import {
   BoxTitleStyled
 } from 'components';
 import {
-  AtTypes,
-  ResultsTypes
-} from 'components/match/components/finalResults/finalResults.types';
+  barChartItems,
+  areaChartItems,
+  goalsAvgAreaChartItems,
+  goalsByTeamAreaChartItems,
+  goalsMaxAndMinAreaChartItems,
+  winsByTeamItems,
+  drawsByTeamItems,
+  lossesByTeamItems
+} from 'config';
 import { AreaChartItemsTypes } from 'components/charts/components/areaChart/areaChart.types';
-import { byPlayed, byNotPLayed, formatMatchesAvgByAt } from 'utils';
+import { byPlayed, byNotPLayed, formatMatchesAvgByAt, addTypesToMatch } from 'utils';
 
 const Ven = () => {
   const played = matches.filter(byPlayed);
   const notPlayed = matches.filter(byNotPLayed);
   const results = resultsAvg[0];
-  const historicalMatches = [
-    ...matchWithMostGoalsFor,
-    ...matchWithMostGoalsAgainst
-  ];
-
+  const historicalMatches = [...matchWithMostGoalsFor, ...matchWithMostGoalsAgainst];
   const MatchesDataFormattedAvgByAt = formatMatchesAvgByAt(matchesAvgByAt);
-
-  const addTypesToMatch = (match: any) => ({
-    ...match,
-    at: match.at as AtTypes,
-    results: match.results as ResultsTypes
-  });
-
-  const barChartItems = [
-    { name: 'Win', fill: '#6DD799' },
-    { name: 'Draw', fill: '#0A1130' },
-    { name: 'Loss', fill: '#D76D6D' }
-  ];
-
-  const areaChartItems = [
-    { name: 'wins', fill: '#6DD799' },
-    { name: 'draws', fill: '#0A1130' },
-    { name: 'losses', fill: '#D76D6D' }
-  ];
-
-  const goalsAvgAreaChartItems = [
-    { name: 'gf', fill: '#6DD799' },
-    { name: 'ga', fill: '#0A1130' }
-  ];
-
-  const goalsByTeamAreaChartItems = [
-    { name: 'gf_sum', fill: '#6DD799' },
-    { name: 'ga_sum', fill: '#0A1130' }
-  ];
-
-  const goalsMaxAndMinAreaChartItems = [
-    { name: 'gf_max', fill: '#6DD799' },
-    { name: 'ga_max', fill: '#0A1130' }
-  ];
-
-  const winsByTeamItems = [{ name: 'wins', fill: '#6DD799' }];
-  const drawsByTeamItems = [{ name: 'draws', fill: '#0A1130' }];
-  const lossesByTeamItems = [{ name: 'losses', fill: '#D76D6D' }];
 
   return (
     <Fragment>
@@ -93,28 +56,18 @@ const Ven = () => {
         <aside>
           <BoxTitleStyled>Próximos partidos</BoxTitleStyled>
           {notPlayed.map((match, index) => (
-            <Match key={index} {...addTypesToMatch(match)} />
+            <Match key={index} {...addTypesToMatch(match as any)} />
           ))}
           <BoxTitleStyled>Goleadas históricas</BoxTitleStyled>
           {historicalMatches.map((match, index) => (
-            <Match key={index} {...addTypesToMatch(match)} />
+            <Match key={index} {...addTypesToMatch(match as any)} />
           ))}
           <BoxTitleStyled>Últimos partidos</BoxTitleStyled>
           {played.map((match, index) => (
-            <Match key={index} {...addTypesToMatch(match)} />
+            <Match key={index} {...addTypesToMatch(match as any)} />
           ))}
         </aside>
         <main>
-          <section>
-            <header>
-              <BoxTitleStyled>Promedio de resultados</BoxTitleStyled>
-            </header>
-            <Charts.BarChart
-              data={MatchesDataFormattedAvgByAt}
-              xAxis="name"
-              items={barChartItems}
-            />
-          </section>
           <section>
             <header>
               <BoxTitleStyled>Promedio de Resultados por año</BoxTitleStyled>
@@ -123,6 +76,16 @@ const Ven = () => {
               data={resultsAvgByYear as []}
               datakey="years"
               items={areaChartItems as AreaChartItemsTypes}
+              customTooltip={({ active, payload }: any) =>
+                active && (
+                  <Charts.Tooltip.ResultsAvgByYear
+                    payload={payload[0].payload}
+                    winColor={payload[0].fill}
+                    drawColor={payload[1].fill}
+                    lossColor={payload[2].fill}
+                  />
+                )
+              }
             />
           </section>
           <section>
@@ -133,6 +96,35 @@ const Ven = () => {
               data={goalsAvgByYear as []}
               datakey="years"
               items={goalsAvgAreaChartItems as any}
+              customTooltip={({ active, payload }: any) =>
+                active && (
+                  <Charts.Tooltip.GoalsAvgByYear
+                    payload={payload[0].payload}
+                    gfColor={payload[0].fill}
+                    gaColor={payload[1].fill}
+                  />
+                )
+              }
+            />
+          </section>
+          <section>
+            <header>
+              <BoxTitleStyled>Promedio de resultados</BoxTitleStyled>
+            </header>
+            <Charts.BarChart
+              data={MatchesDataFormattedAvgByAt}
+              xAxis="name"
+              items={barChartItems}
+              customTooltip={({ active, payload }: any) =>
+                active && (
+                  <Charts.Tooltip.ResultsAvg
+                    name={payload[0].payload.name}
+                    win={payload[0].payload.Win}
+                    draw={payload[0].payload.Draw}
+                    loss={payload[0].payload.Loss}
+                  />
+                )
+              }
             />
           </section>
           <section>
@@ -143,15 +135,16 @@ const Ven = () => {
               data={goalsByTeam as []}
               xAxis="versus"
               items={goalsByTeamAreaChartItems as any}
+              customTooltip={({ active, payload }: any) =>
+                active && (
+                  <Charts.Tooltip.ValuesForAndAgainst
+                    versus={payload[0].payload.versus}
+                    valueFor={payload[0].payload.gf_sum}
+                    valueAgainst={payload[0].payload.ga_sum}
+                  />
+                )
+              }
             />
-            {goalsByTeamAvg.map((item, index) => (
-              <div key={index}>
-                <p>
-                  {item.types}. Mean: {item.mean}. Median: {item.median}. Min:{' '}
-                  {item.min}. Max: {item.max}. Variance: {item.variance}{' '}
-                </p>
-              </div>
-            ))}
           </section>
           <section>
             <header>
@@ -163,6 +156,15 @@ const Ven = () => {
               data={goalsMaxAndMin as []}
               xAxis="versus"
               items={goalsMaxAndMinAreaChartItems as any}
+              customTooltip={({ active, payload }: any) =>
+                active && (
+                  <Charts.Tooltip.ValuesForAndAgainst
+                    versus={payload[0].payload.versus}
+                    valueFor={payload[0].payload.gf_max}
+                    valueAgainst={payload[0].payload.ga_max}
+                  />
+                )
+              }
             />
           </section>
           <section>
@@ -173,6 +175,15 @@ const Ven = () => {
               data={winsByTeam as []}
               xAxis="versus"
               items={winsByTeamItems as any}
+              customTooltip={({ active, payload }: any) =>
+                active && (
+                  <Charts.Tooltip.ByTeam
+                    versus={payload[0].payload.versus}
+                    name={payload[0].name}
+                    type={payload[0].payload['wins']}
+                  />
+                )
+              }
             />
           </section>
           <section>
@@ -180,9 +191,18 @@ const Ven = () => {
               <BoxTitleStyled>Empates por equipo</BoxTitleStyled>
             </header>
             <Charts.BarChart
-              data={drawByTeam as []}
+              data={drawsByTeam as []}
               xAxis="versus"
               items={drawsByTeamItems as any}
+              customTooltip={({ active, payload }: any) =>
+                active && (
+                  <Charts.Tooltip.ByTeam
+                    versus={payload[0].payload.versus}
+                    name={payload[0].name}
+                    type={payload[0].payload['draws']}
+                  />
+                )
+              }
             />
           </section>
           <section>
@@ -193,6 +213,15 @@ const Ven = () => {
               data={lossesByTeam as []}
               xAxis="versus"
               items={lossesByTeamItems as any}
+              customTooltip={({ active, payload }: any) =>
+                active && (
+                  <Charts.Tooltip.ByTeam
+                    versus={payload[0].payload.versus}
+                    name={payload[0].name}
+                    type={payload[0].payload['losses']}
+                  />
+                )
+              }
             />
           </section>
         </main>
