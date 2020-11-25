73c070e65689f91ed1bd2a443b43ad254191c967
manuelitox <manuel2788@gmail.com>
2020-11-25 22:48:31 +0100

    starting to use HashRouter instead of BrowserRouter

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,16 +1,16 @@
 import React, { FC } from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { HashRouter, Route, Switch, Redirect } from 'react-router-dom';
 
 import * as Modules from 'modules';
 
 const Routes: FC = () => (
-  <Router>
+  <HashRouter basename="/">
     <Switch>
       <Route exact path="/" render={() => <Redirect to="/equipos/ven" />} />
       <Route exact path="/equipos/ven" component={Modules.Ven} />
       <Route path="*" render={() => <div>404. Not found.</div>} />
     </Switch>
-  </Router>
+  </HashRouter>
 );
 
 export default Routes;
